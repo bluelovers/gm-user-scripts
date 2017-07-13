ca7ad6d27965be548c7fbf5fdae4ad2149aa13b2
bluelovers <codelovers@users.sourceforge.net>
2017-07-13 20:07:29 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	dist/ux-tweak-sc.user.js
    	dist/ux-tweak-sc.user.js.map
    	gulpfile.js
    	src/lib/parse_url.js
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/dm5.js
    	webpack.config.js

---
+++ dist/ux-tweak-sc.user.js
@@ -0,0 +1,261 @@
+// ==UserScript==
+// @name		ux-tweak-sc
+// @namespace	bluelovers
+//
+// @include		http*://www.dm5.com/*
+//
+// @version		1.0.0
+// @grant		GM_registerMenuCommand
+//
+// require		https://code.jquery.com/jquery-3.2.1.js?1499946934484
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1499946934484
+//
+// ==/UserScript==
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!*********************************!*\
+  !*** ./src/ux-tweak-sc.user.js ***!
+  \*********************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {(function ($, undefined)
+{
+	global.jQuery = global.$ = $;
+
+	$(() =>
+	{
+		'use strict';
+
+		const parse_url = __webpack_require__(/*! ./lib/parse_url */ 2);
+
+		global._url = window.location.href;
+		global._url_obj = global._url_obj_ = parse_url(global._url);
+
+		try
+		{
+			__webpack_require__(/*! ./ux-tweak-sc/dm5 */ 3);
+		}
+		catch (e)
+		{
+			console.error(e.message, e.stack);
+		}
+		finally
+		{
+			console.info([global._url, global._url_obj, global.unsafeWindow]);
+		}
+	});
+})(jQuery.noConflict());
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! ./../node_modules/webpack/buildin/global.js */ 1)))
+
+/***/ }),
+/* 1 */
+/*!***********************************!*\
+  !*** (webpack)/buildin/global.js ***!
+  \***********************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+/* 2 */
+/*!******************************!*\
+  !*** ./src/lib/parse_url.js ***!
+  \******************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+/**
+ * Created by user on 2017/7/13/013.
+ */
+
+module.exports = function parse_url(str, component)
+{
+	// http://kevin.vanzonneveld.net
+	// +      original by: Steven Levithan (http://blog.stevenlevithan.com)
+	// + reimplemented by: Brett Zamir (http://brett-zamir.me)
+	// + input by: Lorenzo Pisani
+	// + input by: Tony
+	// + improved by: Brett Zamir (http://brett-zamir.me)
+	// %          note: Based on http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
+	// %          note: blog post at http://blog.stevenlevithan.com/archives/parseuri
+	// %          note: demo at http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
+	// %          note: Does not replace invalid characters with '_' as in PHP, nor does it return false with
+	// %          note: a seriously malformed URL.
+	// %          note: Besides function name, is essentially the same as parseUri as well as our allowing
+	// %          note: an extra slash after the scheme/protocol (to allow file:/// as in PHP)
+	// *     example 1: parse_url('http://username:password@hostname/path?arg=value#anchor');
+	// *     returns 1: {scheme: 'http', host: 'hostname', user: 'username', pass: 'password', path: '/path', query: 'arg=value', fragment: 'anchor'}
+	var key = [
+			'source', 'scheme', 'authority', 'userInfo', 'user', 'pass', 'host', 'port',
+			'relative', 'path', 'directory', 'file', 'query', 'fragment'
+		],
+		ini = (this.php_js && this.php_js.ini) || {},
+		mode = (ini['phpjs.parse_url.mode'] && ini['phpjs.parse_url.mode'].local_value) || 'php',
+		parser = {
+			php: /^(?:([^:\/?#]+):)?(?:\/\/()(?:(?:()(?:([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?()(?:(()(?:(?:[^?#\/]*\/)*)()(?:[^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+			strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+			loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/\/?)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/ // Added one optional slash to post-scheme to catch file:/// (should restrict this)
+		};
+
+	var m = parser[mode].exec(str),
+		uri = {},
+		i = 14;
+	while (i--)
+	{
+		if (m[i])
+		{
+			uri[key[i]] = m[i];
+		}
+	}
+
+	if (component)
+	{
+		return uri[component.replace('PHP_URL_', '').toLowerCase()];
+	}
+	if (mode !== 'php')
+	{
+		var name = (ini['phpjs.parse_url.queryKey'] && ini['phpjs.parse_url.queryKey'].local_value) || 'queryKey';
+		parser = /(?:^|&)([^&=]*)=?([^&]*)/g;
+		uri[name] = {};
+		uri[key[12]].replace(parser, function ($0, $1, $2)
+		{
+			if ($1)
+			{
+				uri[name][$1] = $2;
+			}
+		});
+	}
+
+	for (i in key)
+	{
+		if (!uri[key[i]])
+		{
+			uri[key[i]] = '';
+		}
+	}
+
+	delete uri.source;
+	return uri;
+}
+;
+
+
+/***/ }),
+/* 3 */
+/*!********************************!*\
+  !*** ./src/ux-tweak-sc/dm5.js ***!
+  \********************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+/**
+ * Created by user on 2017/7/13/013.
+ */
+
+if (_url_obj.host.match(/dm5\.com/))
+{
+	if ($('#checkAdult').length)
+	{
+		$('#checkAdult')[0].click();
+	}
+
+	$('#checkAdult').size()
+}
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=ux-tweak-sc.user.js.map
