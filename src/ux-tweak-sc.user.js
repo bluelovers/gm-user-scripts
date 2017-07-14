4dee178617346dbfb918a3b6a23547e5e94cadda
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 09:11:38 +0800

    . (Step: 10)
    Step: 10
    
    [A ] new file:
    
    	src/lib/dom/disable_nocontextmenu.js
    	src/lib/dom/filter/link.js
    	src/lib/event.done.js
    	src/lib/jquery/scrollTo.js
    	src/ux-tweak-sc/index.js
    
    [D ] deleted:
    
    	dist/ux-tweak-sc.user.js.map
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/dm5.js
    	webpack.config.js

--- src/ux-tweak-sc.user.js
+++ src/ux-tweak-sc.user.js
@@ -1,19 +1,17 @@
+global.jQuery = this.$ = this.jQuery = jQuery.noConflict();
+//global.Promise = require('bluebird');
+
 (function ($, undefined)
 {
-	global.jQuery = global.$ = $;
+	global.$ = $;
 
 	$(() =>
 	{
-		'use strict';
-
-		const parse_url = require('./lib/parse_url');
-
-		global._url = window.location.href;
-		global._url_obj = global._url_obj_ = parse_url(global._url);
-
 		try
 		{
-			require('./ux-tweak-sc/dm5');
+			_init();
+
+			require('./ux-tweak-sc');
 		}
 		catch (e)
 		{
@@ -24,4 +22,17 @@
 			console.info([global._url, global._url_obj, global.unsafeWindow]);
 		}
 	});
-})(jQuery.noConflict());
+})(jQuery);
+
+function _init()
+{
+	const parse_url = require('./lib/parse_url');
+
+	global._url = window.location.href;
+	global._url_obj = global._url_obj_ = parse_url(global._url);
+
+	let imagesLoaded = require('imagesloaded');
+	imagesLoaded.makeJQueryPlugin(jQuery);
+
+	require('./lib/jquery/scrollTo').makeJQueryPlugin(jQuery);
+}
