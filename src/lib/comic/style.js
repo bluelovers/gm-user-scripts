127f22aaebb2168c1e4e0561b78dde008bf96c55
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 13:15:39 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/lib/comic/style.js

---
+++ src/lib/comic/style.js
@@ -0,0 +1,22 @@
+/**
+ * Created by user on 2017/7/14/014.
+ */
+
+module.exports = {
+
+	photo: {
+		'filter': 'contrast(115%)',
+		padding: 0,
+		margin: 'auto',
+	},
+
+	body: {
+		margin: 0,
+		padding: 0,
+	},
+
+	bg_dark: {
+		'background': '#34353b',
+	},
+
+};
