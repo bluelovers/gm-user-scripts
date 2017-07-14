ec8b5abf8a07e4d6657aea8245e3aa8fb2f0dcea
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 17:42:08 +0800

    pixiv (Step: 7)
    Step: 7
    
    [R ] renamed:
    
    	src/ux-tweak-sc/dm5.js -> src/ux-tweak-sc/comic/dm5.js
    
    [A ] new file:
    
    	src/lib/dom/img/size.js
    	src/ux-tweak-sc/comic/exhentai.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/index.js

--- src/ux-tweak-sc/index.js
+++ src/ux-tweak-sc/index.js
@@ -1,1 +1,6 @@
-require('./dm5.js').main();
+console.log('art/pixiv.js');
+require('./art/pixiv.js').main();
+console.log('comic/dm5.js');
+require('./comic/dm5.js').main();
+console.log('comic/exhentai.js');
+require('./comic/exhentai.js').main();
