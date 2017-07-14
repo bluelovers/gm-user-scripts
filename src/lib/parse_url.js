86a1e2dfe629301f0b35710b7acb1e9dd49731b5
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 18:58:51 +0800

    dmzj (Step: 7)
    Step: 7
    
    [A ] new file:
    
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/dmzj.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/lib/comic/style.js
    	src/lib/dom/disable_nocontextmenu.js
    	src/lib/parse_url.js
    	src/ux-tweak-sc/index.js

--- src/lib/parse_url.js
+++ src/lib/parse_url.js
@@ -4,71 +4,97 @@
 
 module.exports = function parse_url(str, component)
 {
-	// http://kevin.vanzonneveld.net
-	// +      original by: Steven Levithan (http://blog.stevenlevithan.com)
-	// + reimplemented by: Brett Zamir (http://brett-zamir.me)
-	// + input by: Lorenzo Pisani
-	// + input by: Tony
-	// + improved by: Brett Zamir (http://brett-zamir.me)
-	// %          note: Based on http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
-	// %          note: blog post at http://blog.stevenlevithan.com/archives/parseuri
-	// %          note: demo at http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
-	// %          note: Does not replace invalid characters with '_' as in PHP, nor does it return false with
-	// %          note: a seriously malformed URL.
-	// %          note: Besides function name, is essentially the same as parseUri as well as our allowing
-	// %          note: an extra slash after the scheme/protocol (to allow file:/// as in PHP)
-	// *     example 1: parse_url('http://username:password@hostname/path?arg=value#anchor');
-	// *     returns 1: {scheme: 'http', host: 'hostname', user: 'username', pass: 'password', path: '/path', query: 'arg=value', fragment: 'anchor'}
+	// eslint-disable-line camelcase
+	//       discuss at: http://locutus.io/php/parse_url/
+	//      original by: Steven Levithan (http://blog.stevenlevithan.com)
+	// reimplemented by: Brett Zamir (http://brett-zamir.me)
+	//         input by: Lorenzo Pisani
+	//         input by: Tony
+	//      improved by: Brett Zamir (http://brett-zamir.me)
+	//           note 1: original by http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
+	//           note 1: blog post at http://blog.stevenlevithan.com/archives/parseuri
+	//           note 1: demo at http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
+	//           note 1: Does not replace invalid characters with '_' as in PHP,
+	//           note 1: nor does it return false with
+	//           note 1: a seriously malformed URL.
+	//           note 1: Besides function name, is essentially the same as parseUri as
+	//           note 1: well as our allowing
+	//           note 1: an extra slash after the scheme/protocol (to allow file:/// as in PHP)
+	//        example 1: parse_url('http://user:pass@host/path?a=v#a')
+	//        returns 1: {scheme: 'http', host: 'host', user: 'user', pass: 'pass', path: '/path', query: 'a=v', fragment: 'a'}
+	//        example 2: parse_url('http://en.wikipedia.org/wiki/%22@%22_%28album%29')
+	//        returns 2: {scheme: 'http', host: 'en.wikipedia.org', path: '/wiki/%22@%22_%28album%29'}
+	//        example 3: parse_url('https://host.domain.tld/a@b.c/folder')
+	//        returns 3: {scheme: 'https', host: 'host.domain.tld', path: '/a@b.c/folder'}
+	//        example 4: parse_url('https://gooduser:secretpassword@www.example.com/a@b.c/folder?foo=bar')
+	//        returns 4: { scheme: 'https', host: 'www.example.com', path: '/a@b.c/folder', query: 'foo=bar', user: 'gooduser', pass: 'secretpassword' }
+	var query
+	var mode = 'php';
 	var key = [
-			'source', 'scheme', 'authority', 'userInfo', 'user', 'pass', 'host', 'port',
-			'relative', 'path', 'directory', 'file', 'query', 'fragment'
-		],
-		ini = (this.php_js && this.php_js.ini) || {},
-		mode = (ini['phpjs.parse_url.mode'] && ini['phpjs.parse_url.mode'].local_value) || 'php',
-		parser = {
-			php: /^(?:([^:\/?#]+):)?(?:\/\/()(?:(?:()(?:([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?()(?:(()(?:(?:[^?#\/]*\/)*)()(?:[^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-			strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-			loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/\/?)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/ // Added one optional slash to post-scheme to catch file:/// (should restrict this)
-		};
-
-	var m = parser[mode].exec(str),
-		uri = {},
-		i = 14;
+		'source',
+		'scheme',
+		'authority',
+		'userInfo',
+		'user',
+		'pass',
+		'host',
+		'port',
+		'relative',
+		'path',
+		'directory',
+		'file',
+		'query',
+		'fragment'
+	]
+	// For loose we added one optional slash to post-scheme to catch file:/// (should restrict this)
+	var parser = {
+		php: new RegExp([
+			'(?:([^:\\/?#]+):)?',
+			'(?:\\/\\/()(?:(?:()(?:([^:@\\/]*):?([^:@\\/]*))?@)?([^:\\/?#]*)(?::(\\d*))?))?',
+			'()',
+			'(?:(()(?:(?:[^?#\\/]*\\/)*)()(?:[^?#]*))(?:\\?([^#]*))?(?:#(.*))?)'
+		].join('')),
+		strict: new RegExp([
+			'(?:([^:\\/?#]+):)?',
+			'(?:\\/\\/((?:(([^:@\\/]*):?([^:@\\/]*))?@)?([^:\\/?#]*)(?::(\\d*))?))?',
+			'((((?:[^?#\\/]*\\/)*)([^?#]*))(?:\\?([^#]*))?(?:#(.*))?)'
+		].join('')),
+		loose: new RegExp([
+			'(?:(?![^:@]+:[^:@\\/]*@)([^:\\/?#.]+):)?',
+			'(?:\\/\\/\\/?)?',
+			'((?:(([^:@\\/]*):?([^:@\\/]*))?@)?([^:\\/?#]*)(?::(\\d*))?)',
+			'(((\\/(?:[^?#](?![^?#\\/]*\\.[^?#\\/.]+(?:[?#]|$)))*\\/?)?([^?#\\/]*))',
+			'(?:\\?([^#]*))?(?:#(.*))?)'
+		].join(''))
+	}
+	var m = parser[mode].exec(str)
+	var uri = {}
+	var i = 14
 	while (i--)
 	{
 		if (m[i])
 		{
-			uri[key[i]] = m[i];
+			uri[key[i]] = m[i]
 		}
 	}
-
 	if (component)
 	{
-		return uri[component.replace('PHP_URL_', '').toLowerCase()];
+		return uri[component.replace('PHP_URL_', '').toLowerCase()]
 	}
 	if (mode !== 'php')
 	{
-		var name = (ini['phpjs.parse_url.queryKey'] && ini['phpjs.parse_url.queryKey'].local_value) || 'queryKey';
-		parser = /(?:^|&)([^&=]*)=?([^&]*)/g;
-		uri[name] = {};
-		uri[key[12]].replace(parser, function ($0, $1, $2)
+		var name = 'queryKey'
+		parser = /(?:^|&)([^&=]*)=?([^&]*)/g
+		uri[name] = {}
+		query = uri[key[12]] || ''
+		query.replace(parser, function ($0, $1, $2)
 		{
 			if ($1)
 			{
-				uri[name][$1] = $2;
+				uri[name][$1] = $2
 			}
-		});
+		})
 	}
-
-	for (i in key)
-	{
-		if (!uri[key[i]])
-		{
-			uri[key[i]] = '';
-		}
-	}
-
-	delete uri.source;
-	return uri;
+	delete uri.source
+	return uri
 }
-;
