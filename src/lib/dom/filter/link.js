bb55368b562855ee525199993a3306ef503fbf28
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 09:11:33 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	src/lib/dom/disable_nocontextmenu.js
    	src/lib/dom/filter/link.js
    	src/lib/event.done.js
    	src/lib/jquery/scrollTo.js
    	src/ux-tweak-sc/index.js
    
    [D ] deleted:
    
    	dist/ux-tweak-sc.user.js.map
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/dm5.js
    	webpack.config.js

---
+++ src/lib/dom/filter/link.js
@@ -0,0 +1,19 @@
+/**
+ * Created by user on 2017/7/13/013.
+ */
+
+module.exports = function _uf_dom_filter_link(_dom, _content, mode)
+{
+	if (_content)
+	{
+		_dom = global.$(_dom, _content);
+	}
+	else
+	{
+		_dom = global.$(_dom);
+	}
+
+	_dom = _dom.filter('a[href]').not('[href^="javascript:"], [href^="#"], [href=""]');
+
+	return _dom;
+};
