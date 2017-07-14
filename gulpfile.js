74538753eaf6ed15be14e83428971b3760f117f4
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 09:11:37 +0800

    . (Step: 8)
    Step: 8
    
    [A ] new file:
    
    	src/lib/dom/disable_nocontextmenu.js
    	src/lib/dom/filter/link.js
    	src/lib/event.done.js
    	src/lib/jquery/scrollTo.js
    	src/ux-tweak-sc/index.js
    
    [D ] deleted:
    
    	dist/ux-tweak-sc.user.js.map
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/dm5.js
    	webpack.config.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,70 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var concat = require('gulp-concat');
 var header = require('gulp-header');
+var sourcemaps = require('gulp-sourcemaps');
 
 var webpack = require("webpack");
 var gulpWebpack = require('webpack-stream');
 
-gulp.task("webpack", function (callback)
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const globby = require('globby');
+
+const path = require('path');
+
+const cwd_src = path.join(__dirname, 'src');
+
+//var closureCompiler = require('google-closure-compiler').gulp();
+
+gulp.task("webpack:before", async function (callback)
+{
+
+	let ls = await globby(['*.user.js'], {
+		cwd: cwd_src,
+	});
+
+	let data = ls.reduce((a, b) =>
+	{
+		let name = path.basename(b, '.user.js')
+
+		a[name] = {};
+
+		return a;
+	}, {});
+
+	for (let name in data)
+	{
+		let ls = await globby(['**.js', '!index.js'], {
+			cwd: path.join(cwd_src, name),
+		});
+
+		//console.log(name, ls, path.join(cwd_src, name));
+
+		data[name] = ls.reduce((a, b) =>
+		{
+			a[b] = require(path.join(cwd_src, name, b))
+
+			return a;
+		}, {});
+	}
+
+	for (let name in data)
+	{
+		let ls = Object.keys(data[name]).reduce((a, b) =>
+		{
+			a.push(`require('./${b}').main();`);
+
+			return a;
+		}, []).join("\n");
+
+		await fs.writeFileAsync(path.join(cwd_src, name, 'index.js'), ls);
+	}
+
+	console.log(data);
+
+});
+
+gulp.task("webpack", ["webpack:before"], function (callback)
 {
 	var pkg = require('./package.json');
 	var banner = `// ==UserScript==
@@ -24,6 +83,8 @@ gulp.task("webpack", function (callback)
 //
 // @version		<%= pkg.version %>
 // @grant		GM_registerMenuCommand
+// @grant		unsafeWindow
+// @grant		GM_addStyle
 //
 // require		https://code.jquery.com/jquery-3.2.1.js?<%= token %>
 // require		https://code.jquery.com/jquery-migrate-3.0.0.js?<%= token %>
@@ -32,14 +93,24 @@ gulp.task("webpack", function (callback)
 `;
 
 	return gulp.src('src/ux-tweak-sc.user.js')
+//		.pipe(sourcemaps.init())
 		.pipe(gulpWebpack(require('./webpack.config.js'), webpack, function (err, stats)
 		{
 			/* Use stats to do more things if needed */
 		}))
+//		.pipe(closureCompiler({
+//			compilation_level: 'SIMPLE',
+//			warning_level: 'VERBOSE',
+//			language_in: 'ECMASCRIPT6_STRICT',
+//			language_out: 'ECMASCRIPT5_STRICT',
+////			output_wrapper: '(function(){\n%output%\n}).call(this)',
+//			js_output_file: 'name.min.js'
+//		}))
 		.pipe(header(banner, {
 			pkg: pkg ,
 			token: Date.now()
 		}))
+//		.pipe(sourcemaps.write('/'))
 		.pipe(gulp.dest('dist/'));
 });
 
