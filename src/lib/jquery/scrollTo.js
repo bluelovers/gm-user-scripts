45bf8f61ee865a0405c74a3dba5e6450a6574d3a
bluelovers <codelovers@users.sourceforge.net>
2017-07-14 09:11:33 +0800

    . (Step: 4)
    Step: 4
    
    [A ] new file:
    
    	src/lib/dom/disable_nocontextmenu.js
    	src/lib/dom/filter/link.js
    	src/lib/event.done.js
    	src/lib/jquery/scrollTo.js
    	src/ux-tweak-sc/index.js
    
    [D ] deleted:
    
    	dist/ux-tweak-sc.user.js.map
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/dm5.js
    	webpack.config.js

---
+++ src/lib/jquery/scrollTo.js
@@ -0,0 +1,56 @@
+/**
+ * Created by user on 2017/7/13/013.
+ */
+
+module.exports.makeJQueryPlugin = ($ = global.jQuery, window = global.window) =>
+{
+	(function (_old)
+	{
+		var _fn_top = function (who)
+		{
+			var _top;
+			var _o;
+
+			if ($.isNumeric(who))
+			{
+				_top = who;
+			}
+			else if ((_o = $(who)) && _o.length)
+			{
+				_top = _o.offset().top;
+			}
+
+			return _top;
+		};
+
+//			if (!$.isfunction(_old))
+		if (typeof _old === 'undefined')
+		{
+			$.fn.scrollTo = function (who, offset)
+			{
+				var _top = _fn_top(who);
+
+				if (offset)
+				{
+					_top = (_top ? _top : 0) + _fn_top(offset);
+				}
+
+				if (_top !== undefined)
+				{
+					this.scrollTop(_top);
+				}
+
+				return this;
+			};
+		}
+
+//			if (!$.isfunction($.scrollTo))
+		if (typeof $.scrollTo === 'undefined')
+		{
+			$.scrollTo = function (who, offset)
+			{
+				return $(global.window).scrollTo(who, offset);
+			};
+		}
+	})($.fn.scrollTo);
+};
