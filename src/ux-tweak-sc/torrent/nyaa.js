de582fd12781f60111a94ed5fc73b9aac6e92a4f
bluelovers <codelovers@users.sourceforge.net>
2017-07-17 01:18:28 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/torrent/jandown.js
    	src/ux-tweak-sc/torrent/nyaa.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/index.js

---
+++ src/ux-tweak-sc/torrent/nyaa.js
@@ -0,0 +1,47 @@
+/**
+ * Created by user on 2017/7/16/016.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://sukebei.nyaa.si/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj)
+	{
+		if (_url_obj.host.match(/sukebei\.nyaa\.si/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main()
+	{
+		const RETURN = module.exports.test(_url_obj);
+
+		//console.log(_url_obj, RETURN);
+
+		if (RETURN)
+		{
+			const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+			_uf_dom_filter_link('.torrent-list tr > td[colspan="2"] a, #torrent-description a')
+				.attr('target', '_blank')
+			;
+
+			return RETURN
+		}
+	},
+
+	adblock()
+	{
+
+	},
+};
