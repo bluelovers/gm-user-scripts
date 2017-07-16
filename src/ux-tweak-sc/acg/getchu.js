708b04587745f4e367ab7cf0058bee69a145b242
bluelovers <codelovers@users.sourceforge.net>
2017-07-17 01:18:27 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/torrent/jandown.js
    	src/ux-tweak-sc/torrent/nyaa.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/index.js

---
+++ src/ux-tweak-sc/acg/getchu.js
@@ -0,0 +1,77 @@
+/**
+ * Created by user on 2017/7/16/016.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://*.getchu.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj)
+	{
+		if (_url_obj.host.match(/getchu\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main()
+	{
+		const RETURN = module.exports.test(_url_obj);
+
+		//console.log(_url_obj, RETURN);
+
+		if (RETURN)
+		{
+			const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+			_uf_dom_filter_link('.product a')
+				.attr('target', '_blank')
+			;
+
+			if (gc_auth())
+			{
+				return RETURN;
+			}
+
+			$('.tablebody img.soft, .highslide > img, .product img')
+				.attr('src', function (i, val)
+				{
+
+					console.log(val);
+
+					return val
+						.replace(/_s\.(jpe?g|bmp|png|gif)$/, '.$1')
+						.replace(/(brandnew\/(?:[^\/]+))\/rc/, '$1/c')
+					;
+				})
+			;
+
+			return RETURN;
+		}
+	},
+
+	adblock()
+	{
+
+	},
+};
+
+function gc_auth()
+{
+	let _img = $('#wrapper > div[style] > img[src*="auth_com3.jpg"]');
+
+	if (_img.length)
+	{
+		location.href = _img.parent().find('a[href*="&gc=gc"]').prop('href');
+
+		return true;
+	}
+}
