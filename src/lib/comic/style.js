b7a9e3c8e6410d5be8277a3a455abfbbac4ed283
bluelovers <codelovers@users.sourceforge.net>
2017-07-16 17:37:34 +0800

    . (Step: 7)
    Step: 7
    
    [A ] new file:
    
    	src/lib/color/jquery.color.js
    	src/lib/color/tag.js
    	src/lib/func/debounce.js
    	src/ux-tweak-sc/torrent/dmhy.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/lib/comic/style.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/index.js

--- src/lib/comic/style.js
+++ src/lib/comic/style.js
@@ -9,15 +9,21 @@ module.exports = {
 		padding: 0,
 		margin: 'auto',
 		border: '0px none #fff',
+		outline: 0,
 	},
 
 	body: {
 		margin: 0,
 		padding: 0,
+		outline: 0,
 	},
 
 	bg_dark: {
 		background: '#34353b',
+	},
+
+	bg_dark_text: {
+		color: '#DDDDDD',
 	},
 
 };
