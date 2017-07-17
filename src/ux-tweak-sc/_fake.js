bf708ece833ca83365627bcae0efcacc8e7740e1
bluelovers <codelovers@users.sourceforge.net>
2017-07-18 00:04:39 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/_fake.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/comic/exhentai.js

---
+++ src/ux-tweak-sc/_fake.js
@@ -0,0 +1,32 @@
+/**
+ * Created by user on 2017/7/17/017.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+		],
+		exclude: [
+			'http*://platform.twitter.com/widgets*',
+			'http*://www.facebook.com/*plugins/*',
+			'http*://staticxx.facebook.com/*',
+			'http*://apis.google.com/*'
+		],
+	},
+
+	test(_url_obj)
+	{
+		return false;
+	},
+
+	main(_url_obj)
+	{
+	},
+
+	adblock(_url_obj)
+	{
+	},
+};
