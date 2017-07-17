8365593ffab35a66f55412601c2366be04a5ba77
bluelovers <codelovers@users.sourceforge.net>
2017-07-17 21:54:19 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/3dmgame.js
    	src/ux-tweak-sc/acg/blog.reimu.net.js
    	src/ux-tweak-sc/google/search.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/lib/comic/style.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/facebook.js

---
+++ src/ux-tweak-sc/acg/3dmgame.js
@@ -0,0 +1,36 @@
+/**
+ * Created by user on 2017/7/17/017.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://www.3dmgame.com/*',
+			'http*://bbs.3dmgame.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj)
+	{
+		if (_url_obj.host.match(/3dmgame\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj)
+	{
+
+	},
+
+	adblock(_url_obj)
+	{
+
+	},
+};
