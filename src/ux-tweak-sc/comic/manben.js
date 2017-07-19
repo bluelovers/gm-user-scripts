33395af6fd8cd0478421e3e2c3b58bfe9d6fe79e
bluelovers <codelovers@users.sourceforge.net>
2017-07-19 23:15:38 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/comic/manben.js
    	src/ux-tweak-sc/shop/pchome.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/index.js

---
+++ src/ux-tweak-sc/comic/manben.js
@@ -0,0 +1,83 @@
+/**
+ * Created by user on 2017/7/19/019.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://www.manben.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/manben\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const keycodes = require('keycodes');
+		const _uf_done = require('../../lib/event.done');
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+
+		$(window)
+			.on('keydown', function (event)
+			{
+				switch (event.which)
+				{
+					case keycodes('pageup'):
+					case keycodes('left'):
+						var _a = $('#crossLeft');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+					case keycodes('pagedown'):
+					case keycodes('right'):
+						var _a = $('#crossRight');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+				}
+			})
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj)
+	{
+		let _dom = $();
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		_dom.remove();
+
+		return _dom;
+	},
+};
