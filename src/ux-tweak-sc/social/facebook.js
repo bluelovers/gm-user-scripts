a449244d6f7df7135ef57804f72275e558597664
bluelovers <codelovers@users.sourceforge.net>
2017-07-19 11:50:12 +0800

    . (Step: 8)
    Step: 8
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/dmm.js
    	src/ux-tweak-sc/code/npmjs.js
    
    [ M] modified - Changes not staged for commit:
    
    	README.md
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/facebook.js

--- src/ux-tweak-sc/social/facebook.js
+++ src/ux-tweak-sc/social/facebook.js
@@ -9,10 +9,9 @@ module.exports = {
 	metadata: {
 		match: [
 			'http*://www.facebook.com/*',
+			'http*://www.facebook.com/saved/*',
 		],
-		exclude: [
-
-		],
+		exclude: [],
 	},
 
 	test(_url_obj)
@@ -27,54 +26,70 @@ module.exports = {
 
 	main()
 	{
-		const RETURN = module.exports.test(_url_obj);
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
 
-		//console.log(_url_obj, RETURN);
-
-		if (RETURN)
+		let _ready = function ()
 		{
+			let _a = $('#appsNav > ul > li > a[data-testid="left_nav_item_建立特效框"]:eq(0)')
+				.not('[data-uf]')
+			;
 
-			let _ready = function ()
+			if (_a.length)
 			{
-				let _a = $('#appsNav > ul > li > a[data-testid="left_nav_item_建立特效框"]:eq(0)')
-					.not('[data-uf]')
-					;
+				_a
+					.attr('data-uf', true)
+					.parent('li')
+					.after(function ()
+					{
+						let li = $('<li />')
+							.addClass('clearfix sideNavItem stat_elem')
+						;
+						let a = $('<a draggable="false" />')
+							.addClass('_5afe')
+							.prop('href', '/profilepicframes?ref=app_bookmark')
+							.html(function ()
+							{
+								return `<span class="imgWrap" data-testid="bookmark_icon_left_nav"><i class="img sp_CgOJqtrmT7M sx_f75c27" draggable="false"></i></span><div dir="ltr" class="linkWrap noCount">使用特效框</div>`;
+							})
+							.appendTo(li)
+						;
 
-				if (_a.length)
-				{
-					_a
-						.attr('data-uf', true)
-						.parent('li')
-						.after(function ()
-						{
-							let li = $('<li />')
-								.addClass('clearfix sideNavItem stat_elem')
-							;
-							let a = $('<a draggable="false" />')
-								.addClass('_5afe')
-								.prop('href', '/profilepicframes?ref=app_bookmark')
-								.html(function ()
-								{
-									return `<span class="imgWrap" data-testid="bookmark_icon_left_nav"><i class="img sp_CgOJqtrmT7M sx_f75c27" draggable="false"></i></span><div dir="ltr" class="linkWrap noCount">使用特效框</div>`;
-								})
-								.appendTo(li)
-							;
+						return li;
+					})
+				;
+			}
 
-							return li;
-						})
-					;
-				}
-			};
+			let _link = _uf_dom_filter_link('#content ._5wcf a._24-t')
+				.not('[data-done]')
+				.attr('data-done', true)
+				.prop('target', '_blank')
+			;
 
-			$('body').on('click', 'a[href]:not([rel="ignore"] or [role] or [href="#"])', function(event)
+			//console.log(_link);
+		};
+
+		$('body')
+			.on('click', 'a[href]:not([rel="ignore"] or [role] or [href="#"])', function (event)
 			{
 				setTimeout(_ready, 3000);
-			});
+			})
+		;
 
-			setTimeout(_ready, 1500);
+		$(window)
+			.on('load', function (event)
+			{
+				setTimeout(_ready, 1000);
+			})
+		;
 
-			return RETURN;
-		}
+		$('#content')
+			.on('DOMNodeInserted', '._5wci._5wch._2pjv, #appsNav', function (event)
+			{
+				setTimeout(_ready, 1000);
+			})
+		;
+
+		setTimeout(_ready, 1500);
 	},
 
 	adblock()
