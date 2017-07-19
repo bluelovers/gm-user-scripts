d5ed5ca1de9b210297ca3b1f524c5e54f7116a8d
bluelovers <codelovers@users.sourceforge.net>
2017-07-19 23:15:38 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	src/ux-tweak-sc/comic/manben.js
    	src/ux-tweak-sc/shop/pchome.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/index.js

---
+++ src/ux-tweak-sc/shop/pchome.js
@@ -0,0 +1,50 @@
+/**
+ * Created by user on 2017/7/19/019.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://mall.pchome.com.tw/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/mall\.pchome\.com\.tw/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj)
+	{
+		let _dom = $();
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		_dom.remove();
+
+		return _dom;
+	},
+};
