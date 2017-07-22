2b05cd4cd7a21b92eb586928f5b29aa1eb516111
bluelovers <codelovers@users.sourceforge.net>
2017-07-22 17:59:39 +0800

    color (Step: 8)
    Step: 8
    
    [R ] renamed:
    
    	src/lib/color/jquery.color.js -> src/lib/jquery/jquery.color.js
    
    [A ] new file:
    
    	src/lib/jquery/history.state.js
    	src/ux-tweak-sc/acg/gamme.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/novel/syosetu.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/ux-tweak-sc/code/npmjs.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/google/search.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/lib/demo.js
    	src/ux-tweak-sc/torrent/dmhy.js

--- src/ux-tweak-sc/code/npmjs.js
+++ src/ux-tweak-sc/code/npmjs.js
@@ -26,9 +26,11 @@ module.exports = {
 	main(_url_obj = global._url_obj)
 	{
 		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
-		_uf_dom_filter_link('#readme a, .box a, a.packageName, b.authorName')
+		_uf_dom_filter_link('#readme a, .box a, a.packageName, a.authorName')
 			.prop('target', '_blank')
 		;
+
+		$(window).scrollTo('.container');
 	},
 
 	adblock(_url_obj = global._url_obj)
