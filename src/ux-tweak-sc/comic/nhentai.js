08ae5180547a99599c1382cbdef134001d4f6270
bluelovers <codelovers@users.sourceforge.net>
2017-07-22 17:59:43 +0800

    color (Step: 11)
    Step: 11
    
    [R ] renamed:
    
    	src/lib/color/jquery.color.js -> src/lib/jquery/jquery.color.js
    
    [A ] new file:
    
    	src/lib/jquery/history.state.js
    	src/ux-tweak-sc/acg/gamme.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/novel/syosetu.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/ux-tweak-sc/code/npmjs.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/google/search.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/lib/demo.js
    	src/ux-tweak-sc/torrent/dmhy.js

--- src/ux-tweak-sc/comic/nhentai.js
+++ src/ux-tweak-sc/comic/nhentai.js
@@ -25,21 +25,71 @@ module.exports = {
 
 	main()
 	{
-		const RETURN = module.exports.test(_url_obj);
+		const keycodes = require('keycodes');
+		const _uf_done = require('../../lib/event.done');
+		const comic_style = require('../../lib/comic/style');
+		const greasemonkey = require('../../lib/greasemonkey');
 
-		//console.log(_url_obj, RETURN);
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('#content .gallery a')
+			.attr('target', '_blank')
+		;
 
-		if (RETURN)
+		const _uf_fixsize2 = require('../../lib/dom/img/size')._uf_fixsize2;
+
+		module.exports.adblock();
+
+		setTimeout(function ()
 		{
-			const _uf_dom_filter_link = require('../../lib/dom/filter/link');
-			_uf_dom_filter_link('#content .gallery a')
-				.attr('target', '_blank')
-			;
-
 			module.exports.adblock();
+		}, 500);
 
-			return RETURN
-		}
+		$(window)
+			.on('resize', function (event)
+			{
+				_uf_fixsize2($('#image-container img'))
+					.css(comic_style.photo)
+				;
+
+				$(window).scrollTo($('#content, #image-container').eq(-1));
+			})
+			.on('keydown', function (event)
+			{
+				switch (event.which)
+				{
+					case keycodes('pageup'):
+					case keycodes('left'):
+						var _a = $('.pagination .previous');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+					case keycodes('pagedown'):
+					case keycodes('right'):
+						var _a = $('.pagination .next');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+				}
+			})
+			.triggerHandler('resize')
+		;
+
+		$('#image-container')
+			.on('DOMNodeInserted', function ()
+			{
+				$(window).triggerHandler('resize');
+			})
+		;
 	},
 
 	adblock()
