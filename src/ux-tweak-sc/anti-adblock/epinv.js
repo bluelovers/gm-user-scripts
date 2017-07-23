fc05874a86e5497306fe68c04b06a2500b5ebe0b
bluelovers <codelovers@users.sourceforge.net>
2017-07-24 00:17:52 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	readme/2017-07-23-15-09-39-1.jpg
    	readme/2017-07-23-15-10-34-2.jpg
    	src/ux-tweak-sc/anti-adblock/epinv.js
    
    [ M] modified - Changes not staged for commit:
    
    	README.md
    	dist/ux-tweak-sc.user.js
    	src/lib/dom/img/size.js
    	src/lib/event/dom.js
    	src/ux-tweak-sc/acg/gamme.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/facebook.js
    	src/ux-tweak-sc/social/weibo.js
    	src/ux-tweak-sc/torrent/dmhy.js

---
+++ src/ux-tweak-sc/anti-adblock/epinv.js
@@ -0,0 +1,63 @@
+/**
+ * Created by user on 2017/7/23/023.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://www.epinv.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/epinv\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		module.exports.adblock(_url_obj);
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+		$(window)
+			.on('load.adblock', function (event)
+			{
+				if ($('#epd .danger').length)
+				{
+					$('#epd')
+						.not('[data-done]')
+						.attr('data-done', true)
+						.html(`<strong>下载地址：</strong><a href="javascript:void(0);" onclick="getDownloadUrl()" ;="">提取下载地址列表</a>`)
+					;
+
+					$('#epd > a[href*="javascript"]').eq(0).click();
+				}
+			})
+			.triggerHandler('load.adblock')
+		;
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
