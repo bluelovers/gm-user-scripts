81bbbeb33e2b7e9db2211fb722085af593a3df22
bluelovers <codelovers@users.sourceforge.net>
2017-07-25 01:22:41 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/wiki/wikipedia.js

---
+++ src/ux-tweak-sc/wiki/wikipedia.js
@@ -0,0 +1,62 @@
+/**
+ * Created by user on 2017/7/24/024.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://*.wikipedia.org/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/wikipedia\.org/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('a.external')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+				'.uls-settings-trigger, .wbc-editpage, .wb-langlinks-edit, .mw-editsection, #p-personal, #p-search',
+				'#footer-icons, #footer-places, .mbox-image .image, #siteNotice',
+			].join())
+		;
+
+		$('.collapseButton')
+			.parents('tr')
+			.siblings()
+			.find(':hidden')
+			.addBack()
+			.show()
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
