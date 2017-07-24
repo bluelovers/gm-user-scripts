6001f2093dfe26855efe9e7c9eea20b6afcd885b
bluelovers <codelovers@users.sourceforge.net>
2017-07-25 01:22:08 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/dlsite.js
    	src/ux-tweak-sc/adult/up01.js
    	src/ux-tweak-sc/art/deviantart.js
    	src/ux-tweak-sc/global/video.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/lib/comic/style.js
    	src/ux-tweak-sc/_fake.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/weibo.js
    	src/ux-tweak-sc/torrent/dmhy.js

---
+++ src/ux-tweak-sc/adult/up01.js
@@ -0,0 +1,70 @@
+/**
+ * Created by user on 2017/7/24/024.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://www.up01.cc/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/up01\.cc/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		if (adult_check(_url_obj))
+		{
+			return true;
+		}
+
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
+
+function adult_check(_url_obj = global._url_obj)
+{
+	let _a = $('#forbid #agree, .adult a.btn.btn-pink');
+
+	if (_a.length)
+	{
+		_a[0].click();
+
+		return true;
+	}
+}
