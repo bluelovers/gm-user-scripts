d471e37380aa85edebe9bc4719ae41762637fdfd
bluelovers <codelovers@users.sourceforge.net>
2017-07-25 01:22:08 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/dlsite.js
    	src/ux-tweak-sc/adult/up01.js
    	src/ux-tweak-sc/art/deviantart.js
    	src/ux-tweak-sc/global/video.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/lib/comic/style.js
    	src/ux-tweak-sc/_fake.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/weibo.js
    	src/ux-tweak-sc/torrent/dmhy.js

---
+++ src/ux-tweak-sc/art/deviantart.js
@@ -0,0 +1,89 @@
+/**
+ * Created by user on 2017/7/24/024.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://*.deviantart.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/deviantart\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('')
+			.prop('target', '_blank')
+		;
+
+		const keycodes = require('keycodes');
+		const _uf_done = require('../../lib/event.done');
+
+		$(window)
+			.on('keydown.page', function (event)
+			{
+				switch (event.which)
+				{
+					case keycodes('pageup'):
+					case keycodes('left'):
+
+						var _a = $('#overhead a.minibrowse_prev:not(.disabled)');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+					case keycodes('pagedown'):
+					case keycodes('right'):
+
+						var _a = $('#overhead a.minibrowse_next:not(.disabled)');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+
+						break;
+				}
+			})
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
