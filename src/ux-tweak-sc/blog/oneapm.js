022423974a1374f6416186d33d66975989a31466
bluelovers <codelovers@users.sourceforge.net>
2017-08-02 05:00:04 +0800

    . (Step: 5)
    Step: 5
    
    [M ] modified:
    
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/global/pagination.js
    
    [A ] new file:
    
    	src/ux-tweak-sc/blog/oneapm.js
    	src/ux-tweak-sc/code/greasyfork.js
    	src/ux-tweak-sc/shop/mall.yahoo.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/acg/reddit.js
    	src/ux-tweak-sc/comic/exhentai.js

---
+++ src/ux-tweak-sc/blog/oneapm.js
@@ -0,0 +1,61 @@
+/**
+ * Created by user on 2017/8/2/002.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://blog.oneapm.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/blog\.oneapm\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add($('#main').parentsUntil('body').addBack().siblings())
+			.add([
+				'.details .readNum, .details .commentNum, .post_info .iconfont',
+				'.msgcontainer, #divSubscibe',
+			].join())
+		;
+
+		require('../../lib/greasemonkey')
+			.GM_addStyle([
+				'#content.with-header-fix { padding-top: 0; }',
+			].join(''))
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
