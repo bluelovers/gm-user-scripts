992d5e9d65ddcc47b7909894d98d6d49e926db00
bluelovers <codelovers@users.sourceforge.net>
2017-08-02 05:00:05 +0800

    . (Step: 6)
    Step: 6
    
    [M ] modified:
    
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/global/pagination.js
    
    [A ] new file:
    
    	src/ux-tweak-sc/blog/oneapm.js
    	src/ux-tweak-sc/code/greasyfork.js
    	src/ux-tweak-sc/shop/mall.yahoo.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/acg/reddit.js
    	src/ux-tweak-sc/comic/exhentai.js

---
+++ src/ux-tweak-sc/code/greasyfork.js
@@ -0,0 +1,53 @@
+/**
+ * Created by user on 2017/8/1/001.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://greasyfork.org/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/greasyfork\.org/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('#browse-script-list article a')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
