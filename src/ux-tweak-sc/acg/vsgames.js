8cbd185578888c08a5f245b66343bc8ac00bcc10
bluelovers <codelovers@users.sourceforge.net>
2017-08-04 14:57:31 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/vsgames.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/global/discuz.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/social/facebook.js

---
+++ src/ux-tweak-sc/acg/vsgames.js
@@ -0,0 +1,56 @@
+/**
+ * Created by user on 2017/8/4/004.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://gw2.vsgames.cn/*',
+			'http*://*.vsgames.cn/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/vsgames\.cn/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add($('#content').parentsUntil('body').addBack().siblings())
+			.add($('#main').appendTo('body').siblings())
+			.add([
+				'.btn-new-topic, .reply-user-avatar, #replytopic, #footer',
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
