5fd23f52491f05642db23cebc93f0c869abe8407
bluelovers <codelovers@users.sourceforge.net>
2017-08-06 11:01:06 +0800

    . (Step: 7)
    Step: 7
    
    [M ] modified:
    
    	src/ux-tweak-sc/_fake.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/shop/yahoo.js
    
    [A ] new file:
    
    	src/ux-tweak-sc/blog/blog.xuite.js
    	src/ux-tweak-sc/global/social.js
    	src/ux-tweak-sc/lib/metadata.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js

---
+++ src/ux-tweak-sc/blog/blog.xuite.js
@@ -0,0 +1,91 @@
+/**
+ * Created by user on 2017/8/6/006.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://blog.xuite.net/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/blog\.xuite\.net/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../lib/dom/filter/link');
+		_uf_dom_filter_link('.blogbody a')
+			.prop('target', '_blank')
+		;
+
+		lazyload(_url_obj)
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add($('.ArticleContent').parentsUntil('body').addBack().siblings())
+			.add([
+				'script, body :hidden',
+
+				'.bookmark.push, #facebook-like-bottom, .fb-comments, .item_trackBack',
+
+				'#sidebarTreeStyle, .selectbar, .commentbutton-menu, .page',
+
+				'link[rel="stylesheet"]',
+			].join())
+		;
+
+		$('.blogrank apan[class]').removeAttr('class');
+
+		$('.ArticleContent')
+			.css({
+				'max-width': 700,
+				margin: 'auto',
+			})
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
+
+function lazyload(_url_obj)
+{
+	$('img.lazyload[data-original]')
+		.not('[data-done]')
+		.attr('data-done', true)
+		.each(function ()
+		{
+			let img = $(this);
+
+			if (img.attr('src') != img.attr('data-original'))
+			{
+				img
+					.attr('src', img.attr('data-original'))
+					.removeAttr('lazyload')
+				;
+			}
+		})
+	;
+}
