b6d16cbe53066423cce6557425e1ee8a2af86607
bluelovers <codelovers@users.sourceforge.net>
2017-08-06 11:01:07 +0800

    . (Step: 8)
    Step: 8
    
    [M ] modified:
    
    	src/ux-tweak-sc/_fake.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/shop/yahoo.js
    
    [A ] new file:
    
    	src/ux-tweak-sc/blog/blog.xuite.js
    	src/ux-tweak-sc/global/social.js
    	src/ux-tweak-sc/lib/metadata.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js

---
+++ src/ux-tweak-sc/global/social.js
@@ -0,0 +1,44 @@
+/**
+ * Created by user on 2017/8/6/006.
+ */
+
+'use strict';
+
+module.exports = {
+
+	priority: 0,
+
+	metadata: {
+		match: [
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		return true;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+				'#facebook-like-bottom, #fb-root',
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
