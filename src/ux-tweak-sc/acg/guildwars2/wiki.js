5500983b5556aaa75e5a649ea59ad6ea5fa1fc87
bluelovers <codelovers@users.sourceforge.net>
2017-08-13 06:48:55 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/guildwars2/forum.js
    	src/ux-tweak-sc/acg/guildwars2/wiki.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/lib/promise/wait.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/global/video.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/lib/demo.js

---
+++ src/ux-tweak-sc/acg/guildwars2/wiki.js
@@ -0,0 +1,64 @@
+/**
+ * Created by user on 2017/8/9/009.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://wiki.guildwars2.com/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if (_url_obj.host.match(/wiki.guildwars2\.com/))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('../../../lib/dom/filter/link');
+
+		let _link = [
+			'a.external',
+		];
+
+		if (_url_obj.path.match(/\/Game_updates\//))
+		{
+			_link.push('#mw-content-text a');
+		}
+
+		_uf_dom_filter_link(_link.join())
+			.not('.mw-editsection a, #toc a')
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
