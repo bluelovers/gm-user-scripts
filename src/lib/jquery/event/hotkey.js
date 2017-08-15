17cfc3b25d476826e1dce53bc2f181011895abf7
bluelovers <codelovers@users.sourceforge.net>
2017-08-15 09:40:22 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/lib/jquery/event/hotkey.js

---
+++ src/lib/jquery/event/hotkey.js
@@ -0,0 +1,27 @@
+/**
+ * Created by user on 2017/8/15/015.
+ */
+
+'use strict';
+
+module.exports.packEvent = function (fn, context = global.window, filter = ':input')
+{
+	return function (event, ...argv)
+	{
+		let _self = this;
+		let d = global.jQuery(document.activeElement);
+
+		if (!d.is(filter))
+		{
+			console.log(':focus', true, d, event);
+
+			let ret = global.jQuery.proxy(fn, _self, event, ...argv)();
+
+			return ret;
+		}
+		else
+		{
+			console.log(':focus', false, d, event);
+		}
+	};
+};
