424df21b2b4f921d05612317f0e356db04220d6d
bluelovers <codelovers@users.sourceforge.net>
2017-08-20 00:24:00 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/qoo-app.js
    	src/ux-tweak-sc/global/pagination/page.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/shop/yahoo.js

---
+++ src/ux-tweak-sc/global/pagination/page.js
@@ -0,0 +1,106 @@
+/**
+ * Created by user on 2017/7/22/022.
+ */
+
+'use strict';
+
+module.exports = {
+
+	priority: 0,
+
+	script: true,
+
+	metadata: {
+		match: [
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		if ($('.pagination > .page-prev, .pagination > .page-next').length)
+		{
+			return 2;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		require('root/src/lib/jquery/event').makeJQueryPlugin($, window);
+
+		let _win = $(window);
+
+		if (_win.eventExists('keydown'))
+		{
+			console.log(_win.events());
+
+			return false;
+		}
+
+		_win
+			.on('keydown.page', require('root/src/lib/jquery/event/hotkey').packEvent(function (event)
+			{
+				const keycodes = require('keycodes');
+				const _uf_done = require('root/src/lib/event.done');
+
+				switch (event.which)
+				{
+					case keycodes('pageup'):
+					case keycodes('left'):
+
+						var _a = $('.pagination > .page-prev');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+						else
+						{
+							console.log(event, _a);
+						}
+
+						break;
+					case keycodes('pagedown'):
+					case keycodes('right'):
+
+						var _a = $('.pagination > .page-next');
+
+						if (_a.length)
+						{
+							_uf_done(event);
+							_a[0].click();
+						}
+						else
+						{
+							console.log(event, _a);
+						}
+
+						break;
+				}
+			}))
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
