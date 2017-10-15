9e95d8454353983d3552d339db68f92f61b97c7b
bluelovers <codelovers@users.sourceforge.net>
2017-10-15 19:50:19 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/qtfy.eu.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	src/lib/greasemonkey/match.js
    	src/ux-tweak-sc/index.js

---
+++ src/ux-tweak-sc/acg/qtfy.eu.js
@@ -0,0 +1,82 @@
+/**
+ * Created by user on 2017/10/15/015.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http*://qtfy.eu/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		let ret;
+
+		if (ret = require('root/src/lib/greasemonkey/match').auto(_url_obj._source, module.exports))
+		{
+			return true;
+		}
+		//console.debug(777, ret);
+
+		/*
+		if (_url_obj.host.match(/3dmgame\.com/))
+		{
+			return true;
+		}
+		*/
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const debounce = require('throttle-debounce/debounce');
+		const throttle = require('throttle-debounce/throttle');
+
+		const _uf_dom_filter_link = require('root/src/lib/dom/filter/link');
+		_uf_dom_filter_link([
+			'#main-layout .class-bar a',
+		].join())
+			.prop('target', '_blank')
+		;
+
+		$(window)
+			.on('load.ready', debounce(6000, function ()
+			{
+				module.exports.adblock(_url_obj);
+			}))
+			.on('load.ready', throttle(1000, function ()
+			{
+				module.exports.adblock(_url_obj);
+			}))
+			.on('load.ready', function ()
+			{
+				module.exports.adblock(_url_obj);
+			})
+			.triggerHandler('load.ready')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+		$('.code-block').filter(':has(> .adblock_wrapper), :has(> .adblock_wrapper)').remove();
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
