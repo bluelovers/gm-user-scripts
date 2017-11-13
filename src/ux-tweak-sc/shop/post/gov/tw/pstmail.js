3952519ffd33efd86bd827c81054ae5d3a0d4473
bluelovers <codelovers@users.sourceforge.net>
2017-11-13 08:57:17 +0800

    修正 Yahoo 購物中心 郵局 貨運單查詢 (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	package-lock.json
    	src/ux-tweak-sc/shop/post/gov/tw/pstmail.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	package.json
    	src/ux-tweak-sc.user.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/lib/metadata.js
    	src/ux-tweak-sc/shop/yahoo/buy.js
    	webpack.config.js

---
+++ src/ux-tweak-sc/shop/post/gov/tw/pstmail.js
@@ -0,0 +1,95 @@
+/**
+ * Created by user on 2017/11/13/013.
+ */
+
+'use strict';
+
+module.exports = {
+
+	metadata: {
+		match: [
+			'http://postserv.post.gov.tw/pstmail/main_mail.html*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		let ret;
+
+		if (ret = require('root/src/lib/greasemonkey/match').auto(_url_obj._source, module.exports))
+		{
+			return true;
+		}
+		//console.debug(777, ret);
+
+		/*
+		if (_url_obj.host.match(/3dmgame\.com/))
+		{
+			return true;
+		}
+		*/
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const debounce = require('throttle-debounce/debounce');
+		const _uf_dom_filter_link = require('root/src/lib/dom/filter/link');
+		_uf_dom_filter_link([].join())
+			.prop('target', '_blank')
+		;
+
+		$('#wrapper').on('DOMNodeInserted', debounce(500, function ()
+		{
+			$(window).triggerHandler('load.ready');
+		}));
+
+		$(window)
+			.on('load.ready', debounce(100, function ()
+			{
+				let input = $('#MAILNO1');
+
+				if (input.length && !input.val())
+				{
+					let r = /MAILNO\d?=(\d+)/;
+
+					if (document.referrer.match(r) || (_url_obj && _url_obj.query.match(r)))
+					{
+						input
+							.val('123')
+							.trigger('keydown')
+							.trigger('keyup')
+							.val(RegExp.$1)
+							.trigger('change')
+						;
+
+						{
+							let event = new Event('change');
+							input[0].dispatchEvent(event);
+						}
+					}
+				}
+			}))
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+};
