0e58f50612db6dbbeb263737bddf952a36d9bf9d
bluelovers <codelovers@users.sourceforge.net>
2017-11-14 14:43:19 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/lib/greasemonkey/uf.ts

---
+++ src/lib/greasemonkey/uf.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by user on 2017/11/14/014.
+ */
+
+import * as greasemonkey from './index';
+
+export function GM_addStyle(css, head?)
+{
+	if (Array.isArray(css))
+	{
+		css = css.join('');
+	}
+
+	if (typeof head == 'object' && head.length && typeof head[0] == 'object')
+	{
+		head = head[0];
+	}
+
+	return greasemonkey.GM_addStyle(css.toString(), head);
+}
