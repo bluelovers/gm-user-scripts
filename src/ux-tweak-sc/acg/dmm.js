1207df67f60fc3e9654294e401f021a978bb3137
bluelovers <codelovers@users.sourceforge.net>
2017-11-17 07:13:32 +0800

    src/lib/event.done.js => src/lib/event/done.ts (Step: 8)
    Step: 8
    
    [A ] new file:
    
    	src/lib/event/done.ts
    	src/ux-tweak-sc/global/pagination/srp_pagination.js
    
    [D ] deleted:
    
    	src/lib/event.done.js
    
    [ M] modified - Changes not staged for commit:
    
    	dist/ux-tweak-sc.user.js
    	package-lock.json
    	package.json
    	src/lib/save.js
    	src/ux-tweak-sc/acg/dmm.js
    	src/ux-tweak-sc/acg/gamer.com.tw.js
    	src/ux-tweak-sc/acg/getchu.js
    	src/ux-tweak-sc/acg/guildwars2/forum.2017.js
    	src/ux-tweak-sc/acg/reddit.js
    	src/ux-tweak-sc/acg/tieba.baidu.js
    	src/ux-tweak-sc/art/deviantart.js
    	src/ux-tweak-sc/art/pixiv.js
    	src/ux-tweak-sc/code/npmjs.js
    	src/ux-tweak-sc/code/stackoverflow.js
    	src/ux-tweak-sc/comic/999comic.js
    	src/ux-tweak-sc/comic/alphapolis.co.jp.js
    	src/ux-tweak-sc/comic/comic-walker.js
    	src/ux-tweak-sc/comic/comico.js
    	src/ux-tweak-sc/comic/dm5.js
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/comic/exhentai.js
    	src/ux-tweak-sc/comic/ikanman.js
    	src/ux-tweak-sc/comic/manben.js
    	src/ux-tweak-sc/comic/nhentai.js
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/global/pagination.js
    	src/ux-tweak-sc/global/pagination/page.js
    	src/ux-tweak-sc/global/pagination/pagination.active.js
    	src/ux-tweak-sc/google/drive.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/shop/yahoo/buy.js
    	src/ux-tweak-sc/shop/yahoo/mall.js
    	src/ux-tweak-sc/social/facebook.js
    	src/ux-tweak-sc/social/weibo.js
    	src/ux-tweak-sc/torrent/dmhy.js

--- src/ux-tweak-sc/acg/dmm.js
+++ src/ux-tweak-sc/acg/dmm.js
@@ -25,7 +25,7 @@ module.exports = {
 
 	main(_url_obj = global._url_obj)
 	{
-		const _uf_done = require('root/src/lib/event.done');
+		const _uf_done = require('root/src/lib/event/done');
 
 		const _uf_dom_filter_link = require('root/src/lib/dom/filter/link');
 		_uf_dom_filter_link('.d-sect .d-item a, a[href*="rcv.ixd.dmm.com"], a[href*="netgame_s"]')
