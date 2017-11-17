33207ac2b91189cf1a036baad2bc0bf992fdc0ac
bluelovers <codelovers@users.sourceforge.net>
2017-11-17 19:48:47 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	lib/core/demo.ts

---
+++ lib/core/demo.ts
@@ -0,0 +1,106 @@
+/**
+ * Created by user on 2017/11/17/017.
+ */
+
+import { IUrlObject } from 'root/lib/func/parse_url';
+
+export interface IDemo
+{
+	disable?: boolean;
+	priority?: number;
+	script?: boolean;
+
+	metadata?: IMetadata,
+
+	test?: IMethodTest;
+	main?: IMethod;
+	adblock?: IMethod;
+	clearly?: IMethodClearly;
+}
+
+export interface IMethod extends Function
+{
+	(_url_obj: IUrlObject);
+}
+
+export interface IMethodTest extends IMethod
+{
+	(_url_obj: IUrlObject): boolean | number;
+}
+
+export interface IMethodClearly extends IMethod
+{
+	(_url_obj: IUrlObject, _dom_list?);
+}
+
+export interface IMetadata
+{
+	match?: string[];
+	exclude?: string[];
+}
+
+export function create(data = {})
+{
+	return Object.assign(
+		{
+
+			metadata: {
+				match: [
+					//'http*://www.3dmgame.com/*',
+				],
+				exclude: [],
+			},
+
+			test(_url_obj = global._url_obj)
+			{
+				let ret;
+
+				if (ret = require('root/lib/greasemonkey/match').auto(_url_obj._source, module.exports))
+				{
+					return true;
+				}
+				//console.debug(777, ret);
+
+				/*
+				if (_url_obj.host.match(/3dmgame\.com/))
+				{
+					return true;
+				}
+				*/
+
+				return false;
+			},
+
+			main(_url_obj = global._url_obj)
+			{
+				const _uf_dom_filter_link = require('root/lib/dom/filter/link');
+				_uf_dom_filter_link([
+					//
+				].join())
+					.prop('target', '_blank')
+				;
+			},
+
+			adblock(_url_obj = global._url_obj)
+			{
+
+			},
+
+			clearly(_url_obj = global._url_obj, _dom_list = null)
+			{
+				let _dom = $(_dom_list);
+
+				_dom = _dom
+					.add([
+						//
+					].join())
+				;
+
+				//_dom.remove();
+
+				return _dom;
+			},
+		}, data);
+}
+
+export default create;
