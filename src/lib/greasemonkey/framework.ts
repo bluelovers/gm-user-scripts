cd830010436eb944a9ea98cdaf9f8bd06ba5ca53
bluelovers <codelovers@users.sourceforge.net>
2017-11-17 12:59:18 +0800

    . (Step: 1)
    Step: 1
    
    [M ] modified:
    
    	src/lib/greasemonkey/framework.ts
    
    [D ] deleted:
    
    	src/lib/greasemonkey/framework.js
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/ux-tweak-sc.user.js
    	package-lock.json
    	package.json
    	src/lib/core/index.ts
    	src/ux-tweak-sc.user.js

--- src/lib/greasemonkey/framework.ts
+++ src/lib/greasemonkey/framework.ts
@@ -4,43 +4,60 @@
 
 'use strict';
 
-module.exports = {
+export interface IRegisterMenuCommandOptions
+{
+	id?: string;
+	key?: string;
 
-	registerMenuCommand(options, callback, argv = [])
+	name?: string;
+	label?: string;
+
+	fn_name?;
+
+	[key: string]: any;
+}
+
+export interface IRegisterMenuCommandCallback extends Function
+{
+	(options: IRegisterMenuCommandOptions);
+}
+
+export function registerMenuCommand(options: string | IRegisterMenuCommandOptions,
+	callback: IRegisterMenuCommandCallback,
+	argv = []
+)
+{
+	if (typeof options == 'string')
 	{
-		if (typeof options == 'string')
+		options = {
+			label: options,
+		} as IRegisterMenuCommandOptions;
+	}
+
+	const label = options.label || `[${options.name || options.id}] ${options.key}`;
+
+	GM_registerMenuCommand(label, function ()
+	{
+		console.time(label);
+		console.group(label);
+
+		let ret;
+
+		try
 		{
-			options = {
-				label: options,
-			};
+			ret = callback(options as IRegisterMenuCommandOptions, ...argv);
+
+			if (ret && ret !== true)
+			{
+				console.info(label, (options as IRegisterMenuCommandOptions).fn_name || callback.name, [ret.length, ret]);
+			}
+		}
+		catch (e)
+		{
+			console.error(e);
 		}
 
-		const label = options.label || `[${options.name || options.id}] ${options.key}`;
-
-		GM_registerMenuCommand(label, function ()
-		{
-			console.time(label);
-			console.group(label);
-
-			let ret;
-
-			try
-			{
-				ret = callback(options, ...argv);
-
-				if (ret && ret !== true)
-				{
-					console.info(label, options.fn_name || callback.name, [ret.length, ret]);
-				}
-			}
-			catch(e)
-			{
-				console.error(e);
-			}
-
-			console.groupEnd(label);
-			console.timeEnd(label);
-		});
-	},
-
-};
+		console.groupEnd(label);
+		console.timeEnd(label);
+	});
+}
