0b6ee45a5feb12e4eca50f982923b27ec93099cf
bluelovers <codelovers@users.sourceforge.net>
2017-11-17 19:48:14 +0800

    . (Step: 3)
    Step: 3
    
    [M ] modified:
    
    	src/dmm-plus-sc/lib/metadata.ts
    
    [A ] new file:
    
    	src/dmm-plus-sc/_fake.ts
    	src/dmm-plus-sc/dmm.co.jp.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/ux-tweak-sc.user.js
    	src/dmm-plus-sc/index.js
    	src/ux-tweak-sc/index.js

---
+++ src/dmm-plus-sc/dmm.co.jp.ts
@@ -0,0 +1,63 @@
+/**
+ * Created by user on 2017/11/17/017.
+ */
+
+import { IDemo } from 'root/lib/core/demo';
+
+'use strict';
+
+let o: IDemo = {
+
+	metadata: {
+		match: [
+			'http*://games.dmm.co.jp/*',
+			'http*://www.dmm.co.jp/netgame/*',
+		],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		let ret;
+
+		if (ret = require('root/lib/greasemonkey/match').auto(_url_obj._source, module.exports))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('root/lib/dom/filter/link');
+		_uf_dom_filter_link([
+			//
+		].join())
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+				//
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+
+};
+
+export = o;
