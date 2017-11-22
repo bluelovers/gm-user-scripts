a28b35834ba3fbdea74cd45b4336c7f6e9f90b54
bluelovers <codelovers@users.sourceforge.net>
2017-11-22 21:35:18 +0800

    . (Step: 5)
    Step: 5
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	src/dmm-plus-sc/index.js
    	src/dmm-plus-sc/index.user.ts
    	src/dmm-plus-sc/lib/metadata.ts
    	src/ux-tweak-sc/index.js

--- src/dmm-plus-sc/index.user.ts
+++ src/dmm-plus-sc/index.user.ts
@@ -7,6 +7,8 @@
 module.exports.id = 'dmm-plus-sc';
 module.exports.name = module.exports.id;
 
+module.exports.desc = 'dmm games layout tweak';
+
 require('root/lib/core').run(module.exports.id, jQuery, function ()
 {
 	require('root/lib/core').init(module.exports, global, window, window.location.href);
