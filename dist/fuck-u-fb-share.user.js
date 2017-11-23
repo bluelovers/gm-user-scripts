48d7735bf6dc0cf00f1d2176a5515d9735c6a752
bluelovers <codelovers@users.sourceforge.net>
2017-11-24 01:41:48 +0800

    . (Step: 2)
    Step: 2
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/ux-tweak-sc.user.js

--- dist/fuck-u-fb-share.user.js
+++ dist/fuck-u-fb-share.user.js
@@ -31,10 +31,10 @@
 //
 // @exclude		
 //
-// require		https://code.jquery.com/jquery-3.2.1.js?1511458782695
-// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511458782695
+// require		https://code.jquery.com/jquery-3.2.1.js?1511458899456
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511458899456
 //
-// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511458782695
+// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511458899456
 //
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
