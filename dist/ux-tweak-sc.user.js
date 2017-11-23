5dc180245baca9e3549e2f6499262e496c899ed5
bluelovers <codelovers@users.sourceforge.net>
2017-11-23 23:04:33 +0800

    . (Step: 2)
    Step: 2
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/ux-tweak-sc.user.js
    	lib/jquery/event/capture.ts
    	src/fuck-u-fb-share/index.js

--- dist/ux-tweak-sc.user.js
+++ dist/ux-tweak-sc.user.js
@@ -125,10 +125,10 @@
 // @exclude		http*://platform.twitter.com/widgets*
 // @exclude		http*://apis.google.com/*
 //
-// require		https://code.jquery.com/jquery-3.2.1.js?1511449262121
-// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511449262121
+// require		https://code.jquery.com/jquery-3.2.1.js?1511449462529
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511449462529
 //
-// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511449262121
+// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511449462529
 //
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
