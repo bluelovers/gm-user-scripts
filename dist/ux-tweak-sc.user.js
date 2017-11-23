e74f0ee2efee3c933f7a1f1307dec93165aa80ea
bluelovers <codelovers@users.sourceforge.net>
2017-11-24 01:40:23 +0800

    粉絲專頁 (Step: 3)
    Step: 3
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/ux-tweak-sc.user.js
    	src/fuck-u-fb-share/facebook/index.ts

--- dist/ux-tweak-sc.user.js
+++ dist/ux-tweak-sc.user.js
@@ -125,10 +125,10 @@
 // @exclude		http*://platform.twitter.com/widgets*
 // @exclude		http*://apis.google.com/*
 //
-// require		https://code.jquery.com/jquery-3.2.1.js?1511458543768
-// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511458543768
+// require		https://code.jquery.com/jquery-3.2.1.js?1511458783020
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511458783020
 //
-// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511458543768
+// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511458783020
 //
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
