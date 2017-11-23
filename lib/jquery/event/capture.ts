922285dd2951635ad938cdb2a7fa4aa491548b04
bluelovers <codelovers@users.sourceforge.net>
2017-11-23 23:03:57 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	lib/jquery/event/capture.ts

---
+++ lib/jquery/event/capture.ts
@@ -0,0 +1,41 @@
+/**
+ * Created by user on 2017/11/23/023.
+ */
+
+export function onCapture(who, originalType: string, listener: EventListener, ...argv)
+{
+	let selector;
+
+	if (typeof listener !== 'function')
+	{
+		selector = listener;
+		listener = argv[argv.length - 1];
+	}
+
+	let t = originalType.split('.');
+
+	const type = t.shift().toString();
+	const namespace = t.join('.').toString();
+
+	let o = $(who)
+		.each(function ()
+		{
+			this.addEventListener(type, function (originalEvent)
+			{
+				//console.debug(type, originalType, selector, listener, originalEvent);
+
+				let event = $.Event(originalEvent);
+
+				if (!selector || selector && $(event.target).is(selector))
+				{
+					console.debug('onCapture', event);
+
+					listener.call(event.target, event);
+				}
+
+			}, true);
+		})
+	;
+}
+
+export default exports;
