f013e1f69c9c6c4de1f1f19678ba4cb767d3d4d7
bluelovers <codelovers@users.sourceforge.net>
2017-11-26 04:19:38 +0800

    . (Step: 9)
    Step: 9
    
    [R ] renamed:
    
    	lib/acg/gamer/signin.ts -> lib/site/gamer/signin.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	src/gamer-auto-signin/ajax.ts
    	src/gamer-auto-signin/index.js
    	src/gamer-auto-signin/index.user.ts
    	src/ux-tweak-sc/index.user.js

--- src/ux-tweak-sc/index.user.js
+++ src/ux-tweak-sc/index.user.js
@@ -154,7 +154,7 @@ function _init_gm()
 		key: '巴哈姆特每日簽到',
 	}, async (options) =>
 	{
-		const signin = require('root/lib/acg/gamer/signin').default;
+		const signin = require('root/lib/site/gamer/signin').default;
 
 		await signin(true)
 			.catch(err => console.error('[簽到錯誤]', err))
