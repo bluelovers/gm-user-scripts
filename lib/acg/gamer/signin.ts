a1daa4dc62be2fd2f23576ce1899b5af4426fb85
bluelovers <codelovers@users.sourceforge.net>
2017-11-26 03:54:18 +0800

    巴哈姆特自動簽到 (Step: 7)
    Step: 7
    
    [M ] modified:
    
    	lib/core/demo.ts
    	lib/core/index.ts
    	lib/dom/img/size.ts
    	lib/event/done.ts
    	lib/greasemonkey/uf.ts
    
    [A ] new file:
    
    	dist/gamer-auto-signin.user.js
    	lib/acg/gamer/signin.ts
    	lib/greasemonkey/gm/xhr.ts
    	src/gamer-auto-signin/ajax.ts
    	src/gamer-auto-signin/index.js
    	src/gamer-auto-signin/index.user.ts
    	src/gamer-auto-signin/lib/metadata.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/ux-tweak-sc.user.js
    	package-lock.json
    	package.json
    	src/fuck-u-fb-share/facebook/index.ts
    	src/fuck-u-fb-share/index.user.ts
    	src/ux-tweak-sc/index.user.js
    	src/ux-tweak-sc/lib/metadata.js

---
+++ lib/acg/gamer/signin.ts
@@ -0,0 +1,121 @@
+/**
+ * Created by user on 2017/11/26/026.
+ */
+
+import GM_XHR, { IXMLHttpRequestResponse } from 'root/lib/greasemonkey/gm/xhr';
+import greasemonkey from 'root/lib/greasemonkey/uf';
+//import * as Promise from 'bluebird';
+
+/**
+ * https://www.gamer.com.tw/ajax/signin.php?_2
+ *
+ * action=2
+ * {"signin":0,"days":5}
+ *
+ * {"signin":1}
+ *
+ * https://www.gamer.com.tw/ajax/get_csrf_token.php?_=1511626146524
+ *
+ * 01d3da5045c096eeb207f1000a85be31b5cbb6ad56465b275a1995a5
+ *
+ * https://www.gamer.com.tw/ajax/signin.php?_1
+ *
+ * action=1
+ token=01d3da5045c096eeb207f1000a85be31b5cbb6ad56465b275a1995a5
+ * {"nowd":6,"days":6,"message":"\u7c3d\u5230\u6210\u529f"}
+ *
+ * {"code":-2,"message":"\u4eca\u5929\u60a8\u5df2\u7d93\u7c3d\u5230\u904e\u4e86\u5594"}
+ */
+export async function signin(force?: boolean): Promise<IXMLHttpRequestResponse>
+{
+	let options = {
+		method: 'POST',
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		dataType: 'json',
+	};
+
+	return GM_XHR
+		.ajax('https://www.gamer.com.tw/ajax/signin.php?_2', Object.assign({}, options, {
+
+			data: {
+				action: 2,
+			},
+
+		}))
+		.then(function (res: IXMLHttpRequestResponse)
+		{
+			//console.log(5, res, res.response);
+
+			if (res && res.response && (force || res.response.signin == 0))
+			{
+				return GM_XHR
+					.ajax('https://www.gamer.com.tw/ajax/get_csrf_token.php', Object.assign({}, options, {
+
+						data: {
+							_: Date.now(),
+						},
+						dataType: 'text',
+
+					}))
+					.then(function (res: IXMLHttpRequestResponse)
+					{
+						//console.log(6, res, res.response);
+
+						if (res && res.response)
+						{
+							return GM_XHR
+								.ajax('https://www.gamer.com.tw/ajax/signin.php?_1', Object.assign({}, options, {
+
+									data: {
+										action: 1,
+										token: res.response,
+									},
+
+								}))
+								.then(function (res: IXMLHttpRequestResponse)
+								{
+									//console.log(7, res, res.response);
+
+									if (res && res.response && res.response.nowd == res.response.days)
+									{
+										greasemonkey.log(`[${res.response.nowd}] ${res.response.message}`, res);
+
+										return res;
+									}
+
+									return Promise.reject(new Error(res));
+								})
+								;
+						}
+
+						greasemonkey.error('無法取得 token', res);
+
+						return Promise.reject(new Error(res));
+					})
+					;
+			}
+			else if (res && res.response &&  res.response.signin == 1)
+			{
+				greasemonkey.log(`已經簽到過`, res);
+
+				return res;
+			}
+
+			return Promise.reject(new Error(res));
+		})
+		.then(function (res)
+		{
+			if (!res || (res !== true) && !res.response)
+			{
+				return Promise.reject(new Error(res));
+			}
+
+			return res;
+		})
+		.catch(err => greasemonkey.error(err))
+		;
+}
+
+export default signin;
