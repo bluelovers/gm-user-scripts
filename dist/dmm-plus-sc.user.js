e18bd291aa3ff3ef454a83403491563396429d9b
bluelovers <codelovers@users.sourceforge.net>
2017-11-26 04:19:35 +0800

    . (Step: 3)
    Step: 3
    
    [R ] renamed:
    
    	lib/acg/gamer/signin.ts -> lib/site/gamer/signin.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	src/gamer-auto-signin/ajax.ts
    	src/gamer-auto-signin/index.js
    	src/gamer-auto-signin/index.user.ts
    	src/ux-tweak-sc/index.user.js

--- dist/dmm-plus-sc.user.js
+++ dist/dmm-plus-sc.user.js
@@ -63,10 +63,10 @@
 // @exclude		*.eu/*
 // @exclude		http*://www.dmm.co*/digital*
 //
-// require		https://code.jquery.com/jquery-3.2.1.js?1511640700549
-// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511640700549
+// require		https://code.jquery.com/jquery-3.2.1.js?1511641144755
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511641144755
 //
-// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511640700549
+// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511641144755
 //
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
