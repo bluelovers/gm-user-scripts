61a9a26a8b0efabb14f2497734b60a7e07ac496e
bluelovers <codelovers@users.sourceforge.net>
2017-11-27 00:08:00 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	lib/core/tpl/demo.ts

---
+++ lib/core/tpl/demo.ts
@@ -0,0 +1,73 @@
+/**
+ * Created by user on 2017/11/26/026.
+ */
+
+/**
+ * Created by user on 2017/11/26/026.
+ */
+
+import { IDemo, IGlobal, IGreasemonkey } from 'root/lib/core/demo';
+
+declare const global: IGlobal;
+declare const greasemonkey: IGreasemonkey;
+
+'use strict';
+
+let o: IDemo = {
+
+	//priority: 100,
+
+	metadata: {
+		match: [
+			//'http*://www.gamer.com.tw/*',
+		],
+		nomatch: [],
+		exclude: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		let ret;
+
+		if (ret = require('root/lib/greasemonkey/match').auto(_url_obj._source, module.exports))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	async main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('root/lib/dom/filter/link');
+		_uf_dom_filter_link([
+			//
+		].join(','))
+			.prop('target', '_blank')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+		//
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+				//
+			].join())
+		;
+
+		//_dom.remove();
+
+		return _dom;
+	},
+
+};
+
+export = o;
+
