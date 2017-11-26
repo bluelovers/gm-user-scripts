94a092efd6e409da1a4585260487e8bd05f68670
bluelovers <codelovers@users.sourceforge.net>
2017-11-26 10:09:58 +0800

    更新說明文字 (Step: 2)
    Step: 2
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	readme/fuck-u-fb-share.md

--- dist/fuck-u-fb-share.user.js
+++ dist/fuck-u-fb-share.user.js
@@ -31,10 +31,10 @@
 //
 // @exclude		
 //
-// require		https://code.jquery.com/jquery-3.2.1.js?1511661588667
-// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511661588667
+// require		https://code.jquery.com/jquery-3.2.1.js?1511661725491
+// require		https://code.jquery.com/jquery-migrate-3.0.0.js?1511661725491
 //
-// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511661588667
+// require		https://raw.githubusercontent.com/bluelovers/jquery-color/develop/jquery.color.js?1511661725491
 //
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
