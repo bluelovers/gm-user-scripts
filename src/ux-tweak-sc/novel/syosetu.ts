960f97687ffec58f27418c9a58891b3bf35d911f
bluelovers <codelovers@users.sourceforge.net>
2017-11-29 03:31:54 +0800

    . (Step: 3)
    Step: 3
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	lib/greasemonkey/match.ts
    	src/ux-tweak-sc/novel/syosetu.ts

--- src/ux-tweak-sc/novel/syosetu.ts
+++ src/ux-tweak-sc/novel/syosetu.ts
@@ -2,21 +2,38 @@
  * Created by user on 2017/7/22/022.
  */
 
+import { IDemo, IGlobal, IGreasemonkey, IWindow, IJQueryStatic, IUrlObject2 } from 'root/lib/core/demo';
+
+declare const global: IGlobal;
+declare const greasemonkey: IGreasemonkey;
+declare const window: IWindow;
+declare const unsafeWindow: IWindow;
+declare const $: IJQueryStatic;
+declare const jQuery: IJQueryStatic;
+
 'use strict';
 
-module.exports = {
+let o: IDemo = {
+
+	//priority: 100,
 
 	metadata: {
 		include: [
 			'http*://syosetu.com/*',
 			'http*://ncode.syosetu.com/*',
 		],
+		match: [],
+		nomatch: [],
 		exclude: [],
+
+		grant: [],
 	},
 
 	test(_url_obj = global._url_obj)
 	{
-		if (_url_obj.host.match(/syosetu\.com/))
+		let ret;
+
+		if (ret = require('root/lib/greasemonkey/match').auto(_url_obj.source, module.exports))
 		{
 			return true;
 		}
@@ -24,24 +41,38 @@ module.exports = {
 		return false;
 	},
 
-	main(_url_obj = global._url_obj)
+	async main(_url_obj = global._url_obj)
 	{
+		const _uf_dom_filter_link = require('root/lib/dom/filter/link');
+		_uf_dom_filter_link([
+			//
+		].join(','))
+			.prop('target', '_blank')
+		;
+
 		$(window).scrollTo('#novel_no', 0 - $('#novel_header').height());
 	},
 
 	adblock(_url_obj = global._url_obj)
 	{
-
+		//
 	},
 
-	clearly(_url_obj = global._url_obj)
+	clearly(_url_obj = global._url_obj, _dom_list = null)
 	{
-		let _dom = $();
+		let _dom = $(_dom_list);
 
 		_dom = _dom
-			.add([].join())
+			.add([
+				//
+			].join())
 		;
+
+		//_dom.remove();
 
 		return _dom;
 	},
+
 };
+
+export = o as IDemo;
