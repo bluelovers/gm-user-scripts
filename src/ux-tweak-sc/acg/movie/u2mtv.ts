142aeee68fce56f38bd29e0d29f9c5ae83f94eaf
bluelovers <codelovers@users.sourceforge.net>
2017-11-29 01:50:59 +0800

    自動嘗試相容不同版本的權限請求 (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/ux-tweak-sc/acg/movie/u2mtv.ts
    
    [ M] modified - Changes not staged for commit:
    
    	dist/dmm-plus-sc.user.js
    	dist/esunbank.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	gulpfile.js
    	lib/func/array/unique.ts
    	lib/greasemonkey/metadata.ts
    	package.json
    	src/dmm-plus-sc/index.js
    	src/dmm-plus-sc/lib/metadata.ts
    	src/esunbank/index.js
    	src/esunbank/lib/metadata.ts
    	src/fuck-u-fb-share/index.js
    	src/fuck-u-fb-share/lib/metadata.ts
    	src/gamer-auto-signin/index.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/lib/metadata.js

---
+++ src/ux-tweak-sc/acg/movie/u2mtv.ts
@@ -0,0 +1,90 @@
+/**
+ * Created by user on 2017/11/28/028.
+ */
+
+import { IDemo, IGlobal, IGreasemonkey, IWindow, IJQueryStatic, IUrlObject2 } from 'root/lib/core/demo';
+
+declare const global: IGlobal;
+declare const greasemonkey: IGreasemonkey;
+declare const window: IWindow;
+declare const unsafeWindow: IWindow;
+declare const $: IJQueryStatic;
+declare const jQuery: IJQueryStatic;
+
+'use strict';
+
+let o: IDemo = {
+
+	metadata: {
+		include: [
+			'http*://www.u2mtv.com/movie/*',
+		],
+		match: [
+			'*://www.u2mtv.com/*',
+		],
+		nomatch: [],
+		exclude: [],
+
+		grant: [],
+	},
+
+	test(_url_obj = global._url_obj)
+	{
+		let ret;
+
+		if (ret = require('root/lib/greasemonkey/match').auto(_url_obj.source, module.exports))
+		{
+			return true;
+		}
+
+		return false;
+	},
+
+	async main(_url_obj = global._url_obj)
+	{
+		const _uf_dom_filter_link = require('root/lib/dom/filter/link');
+		const debounce = require('throttle-debounce/debounce');
+		const throttle = require('throttle-debounce/throttle');
+
+		$('#movie_grid')
+			.on('DOMNodeInserted', debounce(100, function ()
+			{
+				$('body').triggerHandler('load.ready');
+			}))
+		;
+
+		$('body')
+			.on('load.ready', debounce(100, function ()
+			{
+				_uf_dom_filter_link([
+					'.og-expanded .og-expander a',
+					'#movie_grid a',
+				].join(','))
+					.prop('target', '_blank')
+				;
+			}))
+			.triggerHandler('load.ready')
+		;
+	},
+
+	adblock(_url_obj = global._url_obj)
+	{
+		//
+	},
+
+	clearly(_url_obj = global._url_obj, _dom_list = null)
+	{
+		let _dom = $(_dom_list);
+
+		_dom = _dom
+			.add([
+				//
+			].join())
+		;
+
+		return _dom;
+	},
+
+};
+
+export = o as IDemo;
