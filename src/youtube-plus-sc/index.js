346cde3ae9974f5beb319c3446065793d4785cc0
bluelovers <codelovers@users.sourceforge.net>
2017-12-01 07:52:27 +0800

    . (Step: 5)
    Step: 5
    
    [ M] modified - Changes not staged for commit:
    
    	dist/youtube-plus-sc.user.js
    	lib/core/tpl/demo.ts
    	lib/site/youtube/index.ts
    	src/youtube-plus-sc/_fake.ts
    	src/youtube-plus-sc/index.js
    	src/youtube-plus-sc/youtube/2016.ts
    	src/youtube-plus-sc/youtube/2016/my_video.ts
    	src/youtube-plus-sc/youtube/2016/watch.ts

--- src/youtube-plus-sc/index.js
+++ src/youtube-plus-sc/index.js
@@ -16,36 +16,36 @@ module.exports.icon = '';
 module.exports.list = [
 	"youtube/2016/my_video.js",
 	"youtube/2016/upload.js",
-	"youtube/2016/watch.js",
-	"youtube/2016.js",
 	"global/script.js"
 ];
 
-module.exports.list_disable = [];
+module.exports.list_disable = [
+	"youtube/2016/watch.js",
+	"youtube/2016.js"
+];
 
 // for webpack, don't use this method
 module.exports._lib = () =>
 {
 	require('root/src/youtube-plus-sc/youtube/2016/my_video.js');
 	require('root/src/youtube-plus-sc/youtube/2016/upload.js');
-	require('root/src/youtube-plus-sc/youtube/2016/watch.js');
-	require('root/src/youtube-plus-sc/youtube/2016.js');
 	require('root/src/youtube-plus-sc/global/script.js');
 };
 
 module.exports.metadata = {};
 module.exports.metadata.include = [
+	"*://www.youtube.com/*",
+	"*://youtube.com/*",
 	"https://www.youtube.com/my_videos*",
-	"http*://www.youtube.com/upload*",
-	"http*://www.youtube.com/watch*"
+	"http*://www.youtube.com/upload*"
 ];
-module.exports.metadata.match = [
-	"*://*.youtube.com/*",
-	"*://youtube.com/*"
-];
+module.exports.metadata.match = [];
 module.exports.metadata.exclude = [
 	"http*://*.youtube.com/embed/*",
-	"http*://*.youtube.com/v/*"
+	"http*://*.youtube.com/v/*",
+	"http*://creatoracademy.youtube.com/*",
+	"http*://artists.youtube.com/*",
+	"http*://kids.youtube.com/*"
 ];
 module.exports.metadata.grant = [
 	"GM.xmlhttpRequest",
