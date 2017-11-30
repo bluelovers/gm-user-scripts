17cb35832f77b48c6a9e1693c0178a1643e719d6
bluelovers <codelovers@users.sourceforge.net>
2017-11-30 10:15:17 +0800

    嘉里大榮物流 (Step: 9)
    Step: 9
    
    [R ] renamed:
    
    	src/ux-tweak-sc/shop/post/gov/tw/pstmail.js -> src/ux-tweak-sc/shop/post/post/gov/tw/pstmail.js
    
    [A ] new file:
    
    	src/ux-tweak-sc/shop/post/kerrytj.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/esunbank.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	lib/event/done.ts
    	src/ux-tweak-sc/comic/wnacg.js
    	src/ux-tweak-sc/index.js
    	src/ux-tweak-sc/shop/yahoo/buy.js

--- lib/event/done.ts
+++ lib/event/done.ts
@@ -2,13 +2,21 @@
  * Created by user on 2017/7/13/013.
  */
 
-function _uf_done(event: JQueryEventObject, mode?: boolean)
+interface Iexports
+{
+	_uf_done(event: JQueryEventObject|Event, mode?: boolean);
+	_uf_done2(event: JQueryEventObject|Event, mode?: boolean);
+
+	default(event: JQueryEventObject|Event, mode?: boolean);
+}
+
+function _uf_done(event: JQueryEventObject|Event, mode?: boolean)
 {
 	event.stopPropagation();
 	if (!mode) event.preventDefault();
 }
 
-function _uf_done2(event: JQueryEventObject, mode?: boolean)
+function _uf_done2(event: JQueryEventObject|Event, mode?: boolean)
 {
 	//event.stopPropagation();
 	event.stopImmediatePropagation();
@@ -20,6 +28,6 @@ Object.assign(_uf_done, {
 	_uf_done,
 	_uf_done2,
 	default: _uf_done,
-});
+}) as Iexports;
 
 export = _uf_done;
