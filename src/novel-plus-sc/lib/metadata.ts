df4cf2162e81f80df668f64ae2d7bad634d47654
bluelovers <codelovers@users.sourceforge.net>
2017-12-05 15:32:39 +0800

    . (Step: 4)
    Step: 4
    
    [A ] new file:
    
    	dist/novel-plus-sc.user.js
    	src/novel-plus-sc/index.js
    	src/novel-plus-sc/index.user.ts
    	src/novel-plus-sc/lib/metadata.ts
    	src/novel-plus-sc/site/dmzj.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/esunbank.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/ux-tweak-sc.user.js
    	dist/youtube-plus-sc.user.js
    	lib/core/index.ts
    	lib/core/tpl/index.user.ts
    	lib/greasemonkey/match.ts
    	lib/greasemonkey/web/xhr.ts
    	package-lock.json
    	package.json
    	webpack.config.js

---
+++ src/novel-plus-sc/lib/metadata.ts
@@ -0,0 +1,48 @@
+/**
+ * Created by user on 2017/8/6/006.
+ */
+
+export const desc = 'novel';
+
+export const grant = [
+	//'GM_registerMenuCommand',
+	'GM_xmlhttpRequest',
+	'unsafeWindow',
+];
+
+export const metadata = `// ==UserScript==
+// @name		<%= index.name %>
+// @name:en		<%= index.name_en %>
+// @name:zh		<%= index.name %>
+// @namespace	bluelovers
+// @author		bluelovers
+//
+// @description		<%= index.desc %>
+// @description:en	<%= index.desc_en %>
+// @description:zh	<%= index.desc %>
+//
+// @version		<%= pkg.version %>
+//
+// @grant		<%= index.grant %>
+// grant		none
+//
+// @icon		<%= index.icon %>
+//
+// @noframes
+// @encoding	utf-8
+//
+// @homepageURL	https://github.com/bluelovers/gm-user-scripts
+// @supportURL	https://github.com/bluelovers/gm-user-scripts/issues
+// @downloadURL	https://github.com/bluelovers/gm-user-scripts/raw/master/dist/<%= index.id %>.user.js
+// @updateURL	https://github.com/bluelovers/gm-user-scripts/raw/master/dist/<%= index.id %>.user.js
+//
+// @include		<%= index.include %>
+//
+// @match		<%= index.match %>
+//
+// @exclude		<%= index.exclude %>
+//
+// ==/UserScript==
+`;
+
+export default metadata;
