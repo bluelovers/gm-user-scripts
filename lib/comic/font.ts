5195cfb48faf94c36ca605552d95081eb1b09236
bluelovers <codelovers@users.sourceforge.net>
2017-12-05 21:00:28 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	lib/comic/font.ts
    	lib/comic/novel.ts
    	lib/novel/text.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/dmm-plus-sc.user.js
    	dist/esunbank.user.js
    	dist/fuck-u-fb-share.user.js
    	dist/gamer-auto-signin.user.js
    	dist/novel-plus-sc.user.js
    	dist/ux-tweak-sc.user.js
    	dist/youtube-plus-sc.user.js
    	src/novel-plus-sc/site/dmzj.ts

---
+++ lib/comic/font.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 2017/12/5/005.
+ */
+
+export const font = {
+
+	NotoSansSC: {
+		name: 'Noto Sans SC',
+		webfont: '//fonts.googleapis.com/earlyaccess/cwtexyen.css',
+	},
+
+};
+
+export default font;
