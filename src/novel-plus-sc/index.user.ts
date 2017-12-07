dc8ad90bebf822a0b8760b970fe9acc2495832e4
bluelovers <codelovers@users.sourceforge.net>
2017-12-07 09:01:54 +0800

    . (Step: 9)
    Step: 9
    
    [A ] new file:
    
    	readme/images/novel-plus-sc/2017-12-07-08-24-33-5.jpg
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	dist/novel-plus-sc.user.js
    	dist/ux-tweak-sc.user.js
    	lib/novel/text.ts
    	package.json
    	readme/novel-plus-sc.md
    	src/novel-plus-sc/index.js
    	src/novel-plus-sc/index.user.ts
    	src/novel-plus-sc/lib/metadata.ts
    	src/novel-plus-sc/site/baidu/tieba.ts
    	src/ux-tweak-sc/comic/dmzj.js
    	src/ux-tweak-sc/index.user.js

--- src/novel-plus-sc/index.user.ts
+++ src/novel-plus-sc/index.user.ts
@@ -16,4 +16,76 @@ module.exports.indexScript = index;
 userScriptCore.run(module.exports.id, module.exports, jQuery, function (uxid, exports, global, window, $jq, _url)
 {
 	userScriptCore.init(uxid, exports, global, window, $jq, _url);
+
+	_init_gm();
 });
+
+function _init_gm()
+{
+	const UF = require('root/lib/greasemonkey/gm/menu');
+
+	UF.registerMenuCommand({
+		id: module.exports.name,
+		key: 'redo',
+	}, async (options) =>
+	{
+		let index = require(`root/src/${module.exports.id}`);
+
+		if (index.current && index.current.length)
+		{
+			const label = UF.getLabel(options);
+
+			let _dom = $();
+
+			let temp = null;
+
+			let list_script = index.current;
+
+			{
+				let a = [];
+				for (let current of list_script)
+				{
+					if (a.includes(current.name))
+					{
+						continue;
+					}
+
+					a.push(current.name);
+
+					for (let fn of ['adblock'])
+					{
+						if (typeof current.lib[fn] == 'function')
+						{
+							let ret = await current.lib[fn](global._url_obj, _dom);
+
+							if (ret && ret !== true)
+							{
+								if (fn == 'clearly')
+								{
+									//_dom = _dom.add(ret);
+
+									if (ret.length)
+									{
+										// allow remove dom from list
+										// need update clearly
+										_dom = ret;
+									}
+
+									//console.log(777, [ret.length, ret], [_dom.length, _dom]);
+								}
+
+								console.info(label, current.name, fn, [ret.length, ret], [_dom.length, _dom]);
+							}
+						}
+					}
+				}
+			}
+
+			console.info(label, [_dom.length, _dom]);
+
+			_dom
+				.remove()
+			;
+		}
+	});
+}
