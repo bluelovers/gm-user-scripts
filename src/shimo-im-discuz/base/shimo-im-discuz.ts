3a3be6b0b4365622026666bad133a4e9bb242c60
bluelovers <codelovers@users.sourceforge.net>
2019-01-03 04:16:46 +0800

    增加一鍵複製

--- src/shimo-im-discuz/base/shimo-im-discuz.ts
+++ src/shimo-im-discuz/base/shimo-im-discuz.ts
@@ -52,6 +52,8 @@ let o: IDemo = {
 		require('root/lib/jquery/jquery.color').makeJQueryPlugin($, window);
 		const UF = require('root/lib/greasemonkey/gm/menu');
 
+		const copyonclick = require('root/lib/func/copy').copyonclick;
+
 		UF.registerMenuCommand({
 			id: module.exports.name,
 			//key: 'kill it',
@@ -69,40 +71,105 @@ let o: IDemo = {
 
 			if (_editor.length)
 			{
-				let _ql_editor = $('> .ql-editor', _editor).clone();
-
 				console.time('_fn_editor');
 
-				0 && $('p', _ql_editor)
-					.each(function (index, elem)
+				let _ql_editor = $('> .ql-editor', _editor).clone();
+
+				$('style, script, link').each(function ()
 					{
-						let _p = $(this);
+						// @ts-ignore
+						this.disabled = true;
+					})
+					.attr('disabled', 'disabled')
+					.remove()
+				;
 
-						$('span[style*=color]', _p)
-							.each(function ()
+				_ql_editor
+					.removeAttr('contenteditable')
+					.removeAttr('class')
+					.removeAttr('style');
+				;
+
+				$(':root').html('<body/>');
+
+				$('html, :root, body')
+					.removeAttr('lang')
+					.removeAttr('style')
+				;
+
+				_ql_editor.attr('id', '_body');
+
+				/**
+				 * 解決不了 chrome 複製/貼上過程的BUG
+				 */
+				0 && $('*[style]', _ql_editor)
+					.each(function ()
+					{
+						let _this = $(this);
+
+						let _style = this.style;
+
+						let _ok: boolean;
+
+//						let _props: Array<keyof typeof _this[0]["style"]>;
+						let _props: Array<string>;
+
+						_props = [
+							'color',
+							'background-color',
+						];
+
+						let s = '';
+
+						_props
+							.forEach(function (_prop)
 							{
-								let _span = $(this);
-
-								let _style = this.style;
-
-								if (_style.color)
+								if (_style[_prop])
 								{
-									let c = $.Color(_style.color);
+									let c = $.Color(_style[_prop]);
 
-									let _font = $('<font></font>');
+									let hex = c.toHexString();
 
-									_font.attr('color', c.toHexString(true));
+//									_this.css(_prop as string, 'initial');
+//
+//									_this.css(_prop as string, hex);
 
-									_span.wrap(_font)
+
+									_style[_prop as string] = '';
+									//_style[_prop as string] = hex;
+
+									s += `;${_prop}: ${hex};`;
+
+									_ok = true;
 								}
-
 							})
 						;
+
+						if (_ok)
+						{
+							_this.attr('style', function (i, old)
+							{
+								return old + s;
+							});
+
+							console.log(_ok, _this, _style);
+						}
 					})
 				;
 
+				$('body')
 				// @ts-ignore
-				$('body').html(_ql_editor);
+					.html(_ql_editor)
+					.prepend(`<div style="padding: 10px; text-align: center;"><button id="_copy">Copy</button></div>`)
+				;
+
+				$('#_copy').attr('onclick', copyonclick(_ql_editor.attr('id')));
+
+				greasemonkey.GM_addStyle([
+					`body > div { margin: auto; max-width: 800px; }`,
+					`p { line-height: 110%; margin-block-start: auto; margin-block-end: auto; }`,
+					`:root, body, html { font-size: 9pt; font-family: initial !important; }`,
+				], $('body'));
 
 				console.timeEnd('_fn_editor');
 			}
